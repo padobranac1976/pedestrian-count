6d9367a1f7c48113dfb0b013aa673f61356b542d
padobranac1976 <84762182+padobranac1976@users.noreply.github.com>
2021-05-26 12:29:55 +1000

    update order of computation
    Changed the way data is accumulated 
    Placed dates first as this can then be used immediately for the top 10 and the computation needs not be repeated.

--- Pedestrians.py
+++ Pedestrians.py
@@ -3,10 +3,10 @@ import pandas as pd
 import sys
 from sodapy import Socrata
 
-BUFFER = 58
+BUFFER = 56
 
 
-def progressbar(it, prefix="", size=30):
+def progressbar(it, prefix="", size=50):
     count = len(it)
 
     def show(j):
@@ -21,87 +21,68 @@ def progressbar(it, prefix="", size=30):
     sys.stdout.flush()
 
 
-def accumulate_pedestrians(df):
-    dates = df["Date_Time"].unique()
-    acc_ped_df = None
-    for i in progressbar(range(len(dates)), "Accumulating Pedestrians: "):
-        frame = df[df["Date_Time"] == dates[i]]
-        pedestrians = sum(df[df["Date_Time"] == dates[i]]["Hourly_Counts"])
+def update_date_time(df, mode):
+    print("-" * BUFFER)
+    print("Converting date / time format to '{}'-ly frequency...".format(mode))
+    print("-" * BUFFER)
+    df_c = df.copy()
+    if mode == "day":
+        dates = np.array([i[:10] for i in df["Date_Time"]])
+        df_c["Date_Time"] = dates
+        df_c = df_c.drop(columns=["ID", "Time", "Sensor_Name"])
+    elif mode == "month":
+        dates = np.array([i[:3]+i[6:10] for i in df["Date_Time"]])
+        df_c["Date_Time"] = dates
+        df_c = df_c.drop(columns=["ID", "Mdate", "Day", "Time", "Sensor_Name"])
+    return df_c
+
+
+def accumulate_pedestrians(df, x, s):
+    sensors = df["Sensor_ID"].unique()
+    top = None
+    for i in range(len(sensors)):
+        frame = df[df["Sensor_ID"] == sensors[i]]
+        pedestrian_count = sum(df[df["Sensor_ID"] == sensors[i]]["Hourly_Counts"])
         new_frame = pd.DataFrame(frame.iloc[0].values).T
         new_frame.columns = frame.columns
         new_frame = new_frame.rename(columns={'Hourly_Counts': 'Cumulative_Counts'})
-        new_frame["Cumulative_Counts"] = pedestrians
+        new_frame["Cumulative_Counts"] = pedestrian_count
+        sensor_info = s[s["sensor_id"] == sensors[i]].drop(columns=["sensor_id", "direction_1", "direction_2"])
+        cols = list(sensor_info.columns)
+        values = list(sensor_info.values[0])
+        new_frame[cols] = values
+        if top is None:
+            top = new_frame
+        else:
+            top = top.append(new_frame)
+    top = top.sort_values("Cumulative_Counts", ascending=False)[:x]
+    return top
+
+
+def combine_df(s, ped, mode, top_x):
+    pedestrian_df = update_date_time(ped, mode)
+    dates = pedestrian_df["Date_Time"].unique()
+    acc_ped_df = None
+    for i in progressbar(range(len(dates)), "Accumulating pedestrians / {}: "):
+        frame = pedestrian_df[pedestrian_df["Date_Time"] == dates[i]]
+        top = accumulate_pedestrians(frame, top_x, s)
+
         if acc_ped_df is None:
-            acc_ped_df = new_frame
+            acc_ped_df = top
         else:
-            acc_ped_df = acc_ped_df.append(new_frame)
-    print("-" * BUFFER)
+            acc_ped_df = acc_ped_df.append(top)
     return acc_ped_df
-
-
-def update_date_time(df, mode):
-    print("-" * BUFFER)
-    print("Converting date / time format to {}...".format(mode))
-    print("-" * BUFFER)
-    if mode == "day":
-        dates = np.array([i[:10] for i in df["Date_Time"]])
-        df["Date_Time"] = dates
-        df = df.drop(columns=["ID", "Time", "Sensor_Name"])
-    elif mode == "month":
-        dates = np.array([i[:3]+i[6:10] for i in df["Date_Time"]])
-        df["Date_Time"] = dates
-        df = df.drop(columns=["ID", "Mdate", "Day", "Time", "Sensor_Name"])
-    return df
-
-
-def combine_df(sensors, pedestrians, mode):
-    sensor_ids = list(sensors["sensor_id"])
-    sensor_ids.sort()
-    pedestrians = update_date_time(pedestrians, mode)
-    all_sensors = None
-    for s in sensor_ids:
-        s_d = sensors[sensors["sensor_id"] == s]["sensor_description"].values[0]
-        sensor_x = pedestrians[pedestrians["Sensor_ID"] == s]
-        print("Processing sensor id {}: {}".format(s, s_d))
-        if not sensor_x.empty:
-            sensor_x = accumulate_pedestrians(sensor_x)
-            sensor_info = sensors[sensors["sensor_id"] == s].drop(columns=["sensor_id", "direction_1", "direction_2"])
-            cols = list(sensor_info.columns)
-            values = list(sensor_info.values[0])
-            for i in range(len(cols)):
-                sensor_x[cols[i]] = values[i]
-            if all_sensors is None:
-                all_sensors = sensor_x
-            else:
-                all_sensors = all_sensors.append(sensor_x)
-        else:
-            print("-" * BUFFER)
-
-    return all_sensors
-
-
-def find_top_10(df, mode):
-    dates = list(df["Date_Time"].unique())
-    all_data = None
-    for i in progressbar(range(len(dates)), "Filtering '{}' Data".format(mode)):
-        date_x = df[df["Date_Time"] == dates[i]]
-        top_10 = date_x.sort_values("Cumulative_Counts", ascending=False)[:10]
-        if all_data is None:
-            all_data = top_10
-        else:
-            all_data = all_data.append(top_10)
-
-    return all_data
 
 
 if __name__ == "__main__":
     run_analysis = True
     open_from_web = False
+
     if open_from_web:
         client = Socrata("data.melbourne.vic.gov.au", None)
 
         sensor_locations = client.get("h57g-5234", limit=100)
-        sensors_df = pd.DataFrame.from_records(sensor_locations)
+        sensor_location_df = pd.DataFrame.from_records(sensor_locations)
 
         pedestrians = client.get("b2ak-trbp", limit=4000000)
         pedestrians_df = pd.DataFrame.from_records(pedestrians)
@@ -109,15 +90,5 @@ if __name__ == "__main__":
         sensor_location_df = pd.read_csv("Pedestrian_Counting_System_-_Sensor_Locations.csv")
         pedestrians_df = pd.read_csv("Pedestrian_Counting_System_-_Monthly__counts_per_hour.csv")
 
-    if run_analysis:
-        monthly_df = combine_df(sensor_location_df, pedestrians_df, "month")
-        monthly_df.to_csv("./monthly.csv", index=False)
-        daily_df = combine_df(sensor_location_df, pedestrians_df, "day")
-        daily_df.to_csv("./daily.csv", index=False)
-
-    else: # stored locally
-        monthly_df = pd.read_csv("monthly.csv")
-        daily_df = pd.read_csv("daily.csv")
-
-    top_10_monthly_df = find_top_10(monthly_df, "month")
-    top_10_daily_df = find_top_10(daily_df, "day")
+    monthly_df = combine_df(sensor_location_df, pedestrians_df, "month", 10)
+    daily_df = combine_df(sensor_location_df, pedestrians_df, "day", 10)
