74eee9dfef0cc8395f3fd0eef25716404dd23048
padobranac1976 <84762182+padobranac1976@users.noreply.github.com>
2021-05-27 12:07:55 +1000

    add command line argument
    Allow to load data locally and connect it to a command line argument

--- Pedestrians.py
+++ Pedestrians.py
@@ -34,58 +34,44 @@ def update_date_time(df, mode):
     """
     print("-" * BUFFER)
     print("Converting date / time format to '{}'-ly frequency...".format(mode))
-    print("-" * BUFFER)
     df_c = df.copy()
     if mode == "day":
         dates = np.array([i[:10] for i in df["Date_Time"]])
         df_c["Date_Time"] = dates
-        df_c = df_c.drop(columns=["ID", "Time", "Sensor_Name"])
+        df_c = df_c.drop(columns=["ID", "Time", "Sensor_Name", "direction_1", "direction_2"])
     elif mode == "month":
         dates = np.array([i[:3]+i[6:10] for i in df["Date_Time"]])
         df_c["Date_Time"] = dates
-        df_c = df_c.drop(columns=["ID", "Mdate", "Day", "Time", "Sensor_Name"])
+        df_c = df_c.drop(columns=["ID", "Mdate", "Day", "Time", "Sensor_Name", "direction_1", "direction_2"])
     return df_c
 
 
-def accumulate_pedestrians(df, x, s):
-    """ Returns pandas data frame containing top x locations
+def accumulate_pedestrians(df, top_x, mode):
+    """ Returns pandas data frame with all relevant data for top x locations
 
-    Top 10 locations identified for a unique date (either day or month).
+    Top 10 locations identified for a unique date (either day or month). Data that is irrelevant or cannot be
+    combined had been dropped from the table
     """
-    sensors = df["Sensor_ID"].unique()
-    top = None
-    for i in range(len(sensors)):
-        frame = df[df["Sensor_ID"] == sensors[i]]
-        pedestrian_count = sum(df[df["Sensor_ID"] == sensors[i]]["Hourly_Counts"])
-        new_frame = pd.DataFrame(frame.iloc[0].values).T
-        new_frame.columns = frame.columns
-        new_frame = new_frame.rename(columns={'Hourly_Counts': 'Cumulative_Counts'})
-        new_frame["Cumulative_Counts"] = pedestrian_count
-        sensor_info = s[s["sensor_id"] == sensors[i]].drop(columns=["sensor_id", "direction_1", "direction_2"])
-        cols = list(sensor_info.columns)
-        values = list(sensor_info.values[0])
-        new_frame[cols] = values
-        if top is None:
-            top = new_frame
-        else:
-            top = top.append(new_frame)
-    top = top.sort_values("Cumulative_Counts", ascending=False)[:x]
-    return top
+    df1 = update_date_time(df, mode)
+    dates = df1["Date_Time"].unique()
 
-
-def combine_df(s, ped, mode, top_x):
-    """ Returns pandas data frame with all relevant data
-
-    Combines data from pedestrian count and sensor location tables and stores it in a single data frame.
-    Makes calls to "update_date_time" and "accumulate_pedestrians" functions
-    """
-    pedestrian_df = update_date_time(ped, mode)
-    dates = pedestrian_df["Date_Time"].unique()
     acc_ped_df = None
+    top = None
     for i in progressbar(range(len(dates)), "Accumulating pedestrians / {}: ".format(mode)):
-        frame = pedestrian_df[pedestrian_df["Date_Time"] == dates[i]]
-        top = accumulate_pedestrians(frame, top_x, s)
-
+        frame = df1[df1["Date_Time"] == dates[i]]
+        sensors = frame["sensor_id"].unique()
+        for j in range(len(sensors)):
+            sensor_info = frame[frame["sensor_id"] == sensors[j]]
+            pedestrian_count = sum(sensor_info["Hourly_Counts"])
+            new_frame = pd.DataFrame(sensor_info.iloc[0].values).T
+            new_frame.columns = sensor_info.columns
+            new_frame = new_frame.rename(columns={'Hourly_Counts': 'Cumulative_Counts'})
+            new_frame["Cumulative_Counts"] = pedestrian_count
+            if top is None:
+                top = new_frame
+            else:
+                top = top.append(new_frame)
+        top = top.sort_values("Cumulative_Counts", ascending=False)[:top_x]
         if acc_ped_df is None:
             acc_ped_df = top
         else:
@@ -94,7 +80,12 @@ def combine_df(s, ped, mode, top_x):
 
 
 if __name__ == "__main__":
-    open_from_web = False
+    arguments = sys.argv
+    if len(arguments) > 1 and arguments[1] == "-l":
+        open_from_web = False
+    else:
+        open_from_web = True
+    print(["Loading data from local files...", "Loading data from the web API..."][open_from_web])
 
     if open_from_web:
         client = Socrata("data.melbourne.vic.gov.au", None)
@@ -108,8 +99,13 @@ if __name__ == "__main__":
         sensor_location_df = pd.read_csv("Pedestrian_Counting_System_-_Sensor_Locations.csv")
         pedestrians_df = pd.read_csv("Pedestrian_Counting_System_-_Monthly__counts_per_hour.csv")
 
-    monthly_df = combine_df(sensor_location_df, pedestrians_df, "month", 10)
-    monthly_df.to_csv("Monthly.csv")
+    pedestrians_df = pedestrians_df.rename(columns={'Sensor_ID': 'sensor_id'})
+    print("Merging data by sensor ID...")
+    all_data_df = pd.merge(sensor_location_df, pedestrians_df, on="sensor_id")
+    all_data_df.to_csv("all_data.csv")
 
-    daily_df = combine_df(sensor_location_df, pedestrians_df, "day", 10)
-    daily_df.to_csv("Daily.csv")
+    monthly_df = accumulate_pedestrians(all_data_df, 10, "month")
+    monthly_df.to_csv("monthly_data.csv")
+
+    daily_df = accumulate_pedestrians(all_data_df, 10, "day")
+    daily_df.to_csv("daily_data.csv")
